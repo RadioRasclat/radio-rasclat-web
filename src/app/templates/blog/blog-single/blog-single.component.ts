506490602a796a07b842d17a412c31964555ec45
Domenik Töfflinger <domenikt96@yahoo.de>
2021-05-04 02:39:52 +0200

    blog-single 404 handler and error notification

--- src/app/templates/blog/blog-single/blog-single.component.ts
+++ src/app/templates/blog/blog-single/blog-single.component.ts
@@ -5,6 +5,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import Vibrant from 'node-vibrant';
 import { Palette } from 'node-vibrant/lib/color';
+import { Blog } from '@app/core/models/Blog';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-blog-single',
@@ -12,7 +14,7 @@ import { Palette } from 'node-vibrant/lib/color';
   styleUrls: ['./blog-single.component.scss'],
 })
 export class BlogSingleComponent implements OnInit {
-  blogPost: any;
+  blogPost: Blog;
   title: any;
   hex: any;
   rgba: any;
@@ -25,7 +27,8 @@ export class BlogSingleComponent implements OnInit {
     private apiService: ApiService,
     private route: ActivatedRoute,
     private router: Router,
-    private titleService: Title
+    private titleService: Title,
+    private toastService: ToastrService
   ) {}
 
   public setTitle({ title }: { title: any }) {
@@ -36,32 +39,40 @@ export class BlogSingleComponent implements OnInit {
     this.sub = this.route.params.subscribe((params) => {
       this.isLoading = true;
       this.id = params.id;
-      this.apiService
-        .getBlogPost({ blogPostId: this.id })
-        .pipe(
-          finalize(() => {
-            this.isLoading = false;
-          })
-        )
-        .subscribe((blogPost) => {
-          this.blogPost = blogPost;
-          this.setTitle({ title: this.blogPost.title });
-          Vibrant.from(
-            'https://cors-proxy.radio-rasclat.com/' + this.blogPost.image
+      if (this.id) {
+        this.apiService
+          .getBlogPost({ blogPostId: this.id })
+          .pipe(
+            finalize(() => {
+              this.isLoading = false;
+            })
           )
-            .getPalette()
-            .then((palette) => {
-              this.hex = palette.Vibrant.hex;
-              this.rgba =
-                'rgba(' +
-                palette.Vibrant.r +
-                ',' +
-                palette.Vibrant.g +
-                ',' +
-                palette.Vibrant.b +
-                ',0.15)';
-            });
-        });
+          .subscribe((blogPost) => {
+            if (blogPost && blogPost.status !== 404) {
+              this.blogPost = blogPost;
+              this.setTitle({ title: this.blogPost.title });
+              Vibrant.from(
+                'https://cors-proxy.radio-rasclat.com/' + this.blogPost.image
+              )
+                .getPalette()
+                .then((palette) => {
+                  this.hex = palette.Vibrant.hex;
+                  this.rgba =
+                    'rgba(' +
+                    palette.Vibrant.r +
+                    ',' +
+                    palette.Vibrant.g +
+                    ',' +
+                    palette.Vibrant.b +
+                    ',0.15)';
+                });
+            } else {
+              this.toastService.info(blogPost.message, blogPost.status, {
+                closeButton: true,
+              });
+            }
+          });
+      }
     });
   }
 }
