a84e1bd599a6c9618e72c82e259df2c275252163
Domenik Töfflinger <domenikt96@yahoo.de>
2021-07-22 19:05:59 +0200

    add Container component and storybook story

---
+++ src/components/Container/Container.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: var(--container-width);
+  padding: 0 2rem;
+  margin: 0 auto;
+`;
+
+export default Container;
---
+++ src/components/Container/Container.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Container from './Container';
+
+const stories = {
+  title: 'Layout'
+};
+
+export default stories;
+
+export const Container1 = () => {
+  return (
+    <Container>
+      Test
+    </Container>
+  );
+};
