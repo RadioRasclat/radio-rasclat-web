0d699e487af4ff442f6cc05860994aa42544c5dd
Domenik Töfflinger <domenikt96@yahoo.de>
2021-07-25 23:57:40 +0200

    add Gradient background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import Box from './components/Box/Box';
 import Container from './components/Container/Container';
 import Header from './components/Header/Header';
+import Gradient from './components/Gradient';
+import styled from 'styled-components';
+import Marquee3k from 'marquee3000';
+
+const Main = styled.div`
+  position: relative;
+  overflow-x: hidden;
+  color: #ffffff;
+`
 
 function App() {
+  Marquee3k.init()
   return (
     <div className="App">
-      <Header/>
-      <Container>
-        <Box>
-          Cookies!
-        </Box>
-      </Container>
+      <Gradient/>
+      <Main>
+        <Header/>
+        <Container>
+          <div className="marquee3k" data-speed="1.25">
+            <h3>Radio Rasclat&nbsp;→&nbsp;alles auf einen Blick&nbsp;→&nbsp;</h3>
+          </div>
+          <Box>
+            Cookies!
+          </Box>
+        </Container>
+      </Main>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,98 +13,55 @@ const Nav = styled.div`
   background-color: #fff;
   border-bottom: 1px solid rgba(0, 0, 0, 0.0975);
   font-family: "Adieu";
+  padding: 0.6rem 0;
 `;
 
-const NavHeader = styled.div`
-  padding: 1rem 0;
-  width: 100%;
+const NavWrapper = styled.div`
   display: flex;
+  flex-basis: auto;
+  flex-shrink: 0;
+  flex-direction: row;
+  -webkit-box-align: center;
   align-items: center;
-`;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
 
-const NavLeft = styled.div`
-  width: 33.333%;
-  text-align: left;
+  :not(:last-child) {
+    margin-right: 24px;
+  }
 
   svg {
     width: 50px;
   }
-`;
+`
 
-const NavCenter = styled.div`
-  width: 33.333%;
-  text-align: center;
-`;
-
-const Input = styled.input`
-  font-size: 16px;
-  border: solid 1px #dbdbdb;
-  border-radius: 3px;
-  color: #262626;
-  padding: 7px 33px;
-  border-radius: 3px;
-  color: #999;
-  cursor: text;
-  font-size: 14px;
-  font-weight: 300;
-  text-align: center;
-  background: #fafafa;
-
-  &:active,
-  &:focus {
-    text-align: left;
-  }
-`;
-
-const NavRight = styled.div`
-  width: 33.333%;
-  text-align: right;
-
-  svg {
-    margin-right: 20px;
-  }
-`;
-
-const NavLink = styled.a``;
+const SearchWrapper = styled.div`
+  position: relative;
+`
 
 function Header() {
   return (
     <Nav>
       <Container>
-        <NavHeader>
-          <NavLeft>
-            <Logo />
-          </NavLeft>
+        <NavWrapper>
+          <Logo />
 
-          <NavCenter>
-            <Input type="text" placeholder="Search" />
-          </NavCenter>
+          <SearchWrapper>
 
-          <NavRight>
-            <NavLink href="#">
-              Broadcasts
-            </NavLink>
+          </SearchWrapper>
 
-            <NavLink href="#">
-              Shows
-            </NavLink>
+          <Tippy
+            content={<span>Tooltip</span>}
+            trigger="click"
+            theme="light"
+            allowHTML="true"
+            animation="scale-subtle"
+            interactive="true"
+          >
+            <button>My button</button>
+          </Tippy>
 
-            <NavLink href="#">
-              Schedule
-            </NavLink>
-
-            <Tippy
-              content={<span>Tooltip</span>}
-              trigger="click"
-              theme="light"
-              allowHTML="true"
-              animation="scale-subtle"
-              interactive="true"
-            >
-              <button>My button</button>
-            </Tippy>
-          </NavRight>
-        </NavHeader>
+        </NavWrapper>
       </Container>
     </Nav>
   );
